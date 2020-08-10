fc0b7e6d8ad6bc2e6e168bf268c3bb70e2f9f1ac
John <johnfg2610@gmail.com>
2020-08-10 09:46:22 +0100

    init

--- gen.ps1
+++ gen.ps1
@@ -1,1 +1,1 @@
-openapi-generator generate -i ./reference/Bookings.v1.yaml -g typescript-axios -o ./out/bookings/typescript
+openapi-generator generate -i ./reference/Bookings.v1.yaml -g typescript-axios -o ./out/typescript-axios/bookings
